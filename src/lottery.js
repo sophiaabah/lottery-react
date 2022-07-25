415683ef1bab6218e73f43f4e8d692a52b9ae7b4
Sophia Abah <sophiaabah@gmail.com>
2022-07-25 18:02:43 +0200

    finished work

---
+++ src/lottery.js
@@ -0,0 +1,62 @@
+// here we are creating an instance of our lottery contract that js can interact with.
+// we need to import our instance of web3 (module) which was already prepared in the web3.js file
+// to interact with an already deployed contract we need its abi and address.
+// i was forced to use abi as a variable name instead of interface. which is what we used in the earlier projects. just a note to u that they refer to the same thing
+import web3 from "./web3";
+
+const address = "0xFe12Aa32487E4c172e403e5485CE92eF58331F7d";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
